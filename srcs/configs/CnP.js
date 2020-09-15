ed7f2ef29344595eb84cd4b344a2dbfebe9552e3
Gabrielle Sant'Anna <g.santanna21@hotmail.com>
2020-09-14 22:36:25 -0300

    ajustes foram feitos

--- srcs/configs/CnP.js
+++ srcs/configs/CnP.js
@@ -6,46 +6,46 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('database');/*criando um database*/
 
 /*criando tabela tarefas*/
-const TAREFAS_SCHEMA = `
-CREATE TABLE IF NOT EXISTS TAREFAS (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    titulo VARCHAR(64),
-    descricao TEXT,
-    status VARCHAR(32)
-)
-`;
-/*inserindo dados na tabela tarefas*/
-const ADD_TAREFAS = `
-INSERT INTO TAREFAS (
-    titulo,
-    descricao,
-    status
-    )
-VALUES ('Yoga', 'Fazer yoga segunda e quarta', 'Continuo'),
-       ('Médico', 'Consulta com Dr. Ayrton sexta', 'TODO'),
-       ('Pagar contas', 'Pagar boletos de água e luz', 'DOING')
-`
+// const TAREFAS_SCHEMA = `
+// CREATE TABLE IF NOT EXISTS TAREFAS (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     titulo VARCHAR(64),
+//     descricao TEXT,
+//     status VARCHAR(32)
+// )
+// `;
+// /*inserindo dados na tabela tarefas*/
+// const ADD_TAREFAS = `
+// INSERT INTO TAREFAS (
+//     titulo,
+//     descricao,
+//     status
+//     )
+// VALUES ('Yoga', 'Fazer yoga segunda e quarta', 'Continuo'),
+//        ('Médico', 'Consulta com Dr. Ayrton sexta', 'TODO'),
+//        ('Pagar contas', 'Pagar boletos de água e luz', 'DOING')
+// `
 /*executando comandos em sequencia*/
-db.serialize( ()=> {
+// db.serialize( ()=> {
 
-    db.run(TAREFAS_SCHEMA, (err)=> {
-        if (err) {
-            console.log('Erro na criação da tabela tarefas');
-            process.exit(1);
-        }
-    });
-    db.run(ADD_TAREFAS, (err) => {
-        if (err) {
-            console.log('Erro ao adicionar valores ao banco');
-            process.exit(1);
-        }
-    })
-
-
-db.all('SELECT * FROM TAREFAS', (err, rows) => {
-    if (err) {throw err}
-    console.log(rows)
-    })
+//     db.run(TAREFAS_SCHEMA, (err)=> {
+//         if (err) {
+//             console.log('Erro na criação da tabela tarefas');
+//             process.exit(1);
+//         }
+//     });
+//     db.run(ADD_TAREFAS, (err) => {
+//         if (err) {
+//             console.log('Erro ao adicionar valores ao banco');
+//             process.exit(1);
+//         }
+//     })
 
 
-})
+// db.all('SELECT * FROM TAREFAS', (err, rows) => {
+//     if (err) {throw err}
+//     console.log(rows)
+//     })
+
+
+// })
